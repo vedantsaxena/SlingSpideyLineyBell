5e834dfbe91ed87c3c615ee131dae3a687e8faa8
vedantsaxena <tanmay.saxena@gmail.com>
2020-05-13 10:54:38 +0530

    cheapSlinger/badSpidey

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var bChai,Logan;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -35,6 +36,9 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    Logan = new Log(100,100,200,PI);
+
+    bChai = new ChaiLeLo(bird.body,Logan.body);
 
 }
 
@@ -61,4 +65,6 @@ function draw(){
 
     bird.display();
     platform.display();
+    Logan.display();
+    bChai.display();
 }
